495acce04a611adbdcadde39d4840a70716f3556
Boris Lohner <bl@sys4.de>
2024-09-25 03:57:12 +0200

    Added and improved documentation

--- sys4_tlsrpt/utility.py
+++ sys4_tlsrpt/utility.py
@@ -1,14 +1,18 @@
 import datetime
 
-
-# DELETEME/REVIEW: No used anywhere in the code. Should be removed
+#  used only for development
 def myprint(*args, **kwargs):
     pass
     return print(*args, **kwargs)
 
-def parse_tlsrpt_record(s):
+def parse_tlsrpt_record(tlsrpt_record):
+    """
+    Parses a TLSRPT DNS record and extracts the destination URIs
+    :param s:
+    :return:
+    """
     # first split into the main parts: version and RUAs
-    mparts = s.split(";")
+    mparts = tlsrpt_record.split(";")
     if(len(mparts) < 2):
         raise Exception("Malformed TLSRPT record: No semicolon found")
     if(mparts[0] != "v=TLSRPTv1"):
@@ -16,16 +20,31 @@ def parse_tlsrpt_record(s):
     ruapart = mparts[1].strip()
     if not ruapart.startswith("rua="):
         raise Exception("Malformed TLSRPT record: No rua found")
-    ruapart=ruapart[4:]
+    ruapart=ruapart[4:]  # remove leading "rua="
     ruas=ruapart.split(",")
     return ruas
 
+def tlsrpt_report_start_datetime(day):
+    """
+    Return start time of report for a specific day.
+    :param day:  Day for which to create the start time.
+    :return: Timestamp of the report start the in the format required by RFC 8460
+    """
+    return day + "T00:00:00Z"
+
+def tlsrpt_report_end_datetime(day):
+    """
+    Return end time of report for a specific day.
+    :param day:  Day for which to create the end time.
+    :return: Timestamp of the report end the in the format required by RFC 8460
+    """
+    return day + "T23:59:59Z"
 
 def tlsrpt_utc_time_now():
     """
     Returns a timezone aware datetime object of the current UTC time.
     """
-    return datetime.datetime.now().astimezone(datetime.timezone.utc)
+    return datetime.datetime.now(datetime.timezone.utc)
 
 
 def tlsrpt_utc_date_now():

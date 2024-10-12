b6d18db6d4dd6bb9b2c3f4420f140b236ec159fc
Boris Lohner <bl@sys4.de>
2024-10-12 07:14:14 +0200

    Fixed location of example.cfg

--- tests/test_example_config.py
+++ tests/test_example_config.py
@@ -1,4 +1,6 @@
 import unittest
+import os
+import sys
 
 from pytlsrpt.tlsrpt import ConfigReceiver, ConfigReporter, options_from_cmd_cfg_env, options_receiver, options_reporter,TLSRPTReceiver, TLSRPTReporter, pospars_fetcher
 
@@ -6,18 +8,23 @@ class MyTestCase(unittest.TestCase):
     """
     Test usability of example config file
     """
-    example_filename = "../pytlsrpt/example.cfg"
+
+    def setUp(self):
+        self.example_filename = os.path.join(os.path.dirname(__file__), "..", "pytlsrpt" , "example.cfg")
+        sys.argv.append("--config_file")
+        sys.argv.append(self.example_filename)
 
     def test_receiver_config(self):
-        (configvars, params) = options_from_cmd_cfg_env(options_receiver, self.example_filename,
+        (configvars, params) = options_from_cmd_cfg_env(options_receiver, TLSRPTReceiver.DEFAULT_CONFIG_FILE,
                                                         TLSRPTReceiver.CONFIG_SECTION,
                                                         TLSRPTReceiver.ENVIRONMENT_PREFIX,
                                                         {})
         config = ConfigReceiver(**configvars)
         self.assertEqual(config.log_level, "debug")
+        self.assertEqual(config.receiver_logfilename, "/tmp/tlsrpt-receiver.log")
 
     def test_fetcher_config(self):
-        (configvars, params) = options_from_cmd_cfg_env(options_receiver, self.example_filename,
+        (configvars, params) = options_from_cmd_cfg_env(options_receiver, TLSRPTReceiver.DEFAULT_CONFIG_FILE,
                                                         TLSRPTReceiver.CONFIG_SECTION,
                                                         TLSRPTReceiver.ENVIRONMENT_PREFIX,
                                                         pospars_fetcher)
@@ -25,12 +32,13 @@ class MyTestCase(unittest.TestCase):
         self.assertEqual(config.log_level, "debug")
 
     def test_reporter_config(self):
-        (configvars, params) = options_from_cmd_cfg_env(options_reporter, self.example_filename,
+        (configvars, params) = options_from_cmd_cfg_env(options_reporter, TLSRPTReporter.DEFAULT_CONFIG_FILE,
                                                         TLSRPTReporter.CONFIG_SECTION,
                                                         TLSRPTReporter.ENVIRONMENT_PREFIX,
                                                         {})
         config = ConfigReporter(**configvars)
         self.assertEqual(config.log_level, "debug")
+        self.assertEqual(config.reporter_logfilename, "/tmp/tlsrpt-reporter.log")
 
 
 if __name__ == '__main__':

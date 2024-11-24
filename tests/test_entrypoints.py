fc456155e0f226cb5da752d55d1af0c8ada6cfae
Boris Lohner <bl@sys4.de>
2024-11-24 22:21:35 +0100

    Added support for additional and alternative storage plugins

---
+++ tests/test_entrypoints.py
@@ -0,0 +1,44 @@
+#
+#    Copyright (C) 2024 sys4 AG
+#    Author Boris Lohner bl@sys4.de
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.
+#    If not, see <http://www.gnu.org/licenses/>.
+#
+
+import unittest
+from pytlsrpt import plugins
+
+class MyTestCase(unittest.TestCase):
+    def plugin_entrypoint(self, group, url, expected_typename):
+        """
+        Test existance of an entrypoint to be loaded as storage back-end
+        :param group: name of the group
+        :param url: URL describing the storage back-end
+        :param expected_typename: expected typename of the loaded storage back-end
+        """
+        cls = plugins.get_plugin(group, url)
+        self.assertEqual(cls.__name__, expected_typename)
+
+    def test_plugin_entrypoints(self):
+        """
+        Test existance and loadability of the defined plugin entrypoints
+        """
+        self.plugin_entrypoint("tlsrpt.receiver", "sqlite:///tmp/test-receiver.sqlite", "TLSRPTReceiverSQLite")
+        self.plugin_entrypoint("tlsrpt.receiver", "dummy://", "DummyReceiver")
+        self.plugin_entrypoint("tlsrpt.fetcher", "sqlite:///tmp/test-receiver.sqlite", "TLSRPTFetcherSQLite")
+
+
+if __name__ == '__main__':
+    unittest.main()
